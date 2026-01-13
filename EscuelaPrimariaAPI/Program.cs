4dab0dbb49f309604a39bb474a22e337eca4c153
fereandao12 <AyanoAguirre@outlook.com>
2026-01-12 19:48:12 -0500

    Cambios program.cs

--- EscuelaPrimariaAPI/Program.cs
+++ EscuelaPrimariaAPI/Program.cs
@@ -3,19 +3,33 @@ using EscuelaPrimariaAPI.Data;
 using EscuelaPrimariaAPI.Mapping;
 using EscuelaPrimariaAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using QuestPDF.Infrastructure; 
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar Licencia de QuestPDF ---
+QuestPDF.Settings.License = LicenseType.Community;
 
 // Add services to the container.
 
 // Configuracion de la cadena de conexion
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// IMPORTANTE: Si usas SmarterASP, a veces necesitas TrustServerCertificate=True
-// Asegúrate de que tu Connection String en Azure tenga eso.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// --- 3. IMPORTANTE: Configurar CORS para Blazor ---
+// Esto permite que tu Frontend (Blazor) descargue archivos de esta API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor", policy =>
+    {
+        policy.AllowAnyOrigin() // En producción, cambia esto por la URL de tu Blazor
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 // Automapper manual config
 var mapingConfig = new MapperConfiguration(mc =>
@@ -33,12 +47,9 @@ builder.Services.AddScoped<IApoderadoService, ApoderadoService>();
 builder.Services.AddScoped<ISeccionService, SeccionService>();
 builder.Services.AddScoped<IImagenService, ImagenService>();
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Solo una llamada a AddControllers con las opciones JSON
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
@@ -49,21 +60,21 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// --- CORRECCIÓN AQUÍ ---
 // Sacamos Swagger del "if" para que funcione en Azure (Producción)
 app.UseSwagger();
 app.UseSwaggerUI();
-// -----------------------
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazor");
+
+app.UseStaticFiles(); 
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseStaticFiles();
-
-// Opcional: Esto hace que al abrir la raiz (/) te lleve directo al Swagger
+// Redirección a Swagger en la raíz
 app.MapGet("/", context => {
     context.Response.Redirect("/swagger/index.html");
     return Task.CompletedTask;
